13295d337babbccbee0ced2204d1f2ad6a979fa6
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-21 10:30:39 -0300

    Nuke Gboard and wellbeing overlays
    Not needed anymore
    
    Change-Id: I7ca5b5ad6ace04b1ecacc378b01678894fb05869

--- config.mk
+++ config.mk
@@ -10,9 +10,6 @@ PRODUCT_PACKAGES += \
     SystemBlack \
     SystemUIDark \
     SystemUIBlack \
-    GBoardDark \
-    GBoardLight \
-    WellbeingTheme \
     DocumentsUIDark \
     DocumentsUIBlack \
     UpdatesDark \
